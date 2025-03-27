a24574abbd772acc9fd23a41591910b571d7c61b
Rakshandhaboro <u22cse1030@cit.ac.in>
2025-03-27 23:43:02 +0530

    Add files via upload

---
+++ N3P/setup.py/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="python_nlp",
+    version="0.1.0",
+    packages=["python_nlp"],
+    install_requires=[
+        'nltk>=3.7',
+        'numpy>=1.21.0',
+        'scikit-learn>=1.0.0',
+    ],
+)
